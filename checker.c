59260c46189e2b608d1b14e7ef125f9a475df2ff
Ramesh-nex <rameshsaravanan16091996@gmail.com>
2024-07-06 20:52:13 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,33 +1,21 @@
 #include <stdio.h>
 #include <assert.h>
 
-int OutOfRange(float val, float min, float max ,int out)
+int OutOfRange(float val, float min, float max ,const char* msg)
 {
-  if (val < min || val > max) {
-         if(out ==1)
+  if (val < min || val > max) 
   {
-    printf("Temperature out of range!\n");
-     return 0;
-  }
-  else if(out ==2)
-  {
-    printf("State of Charge out of range!\n");
-     return 0;
-  }
-  else
-  {
-    printf("Charge Rate out of range!\n");
-     return 0;
+    printf("%s\n",msg);
+    return 0;
   }
 
-    }
     return 1;
 }
 int batteryIsOk(float temperature, float soc, float chargeRate)
 {
-   return OutOfRange(temperature,0,45,1)
-      && OutOfRange(soc,20,80,2)
-      && OutOfRange(chargeRate,0,0.8,3);
+   return OutOfRange(temperature,0,45,"Temperature out of range!")
+      && OutOfRange(soc,20,80,"SOC out of range!")
+      && OutOfRange(chargeRate,0,0.8,"Charge Rate out of range!");
 }
 
 int main() {
