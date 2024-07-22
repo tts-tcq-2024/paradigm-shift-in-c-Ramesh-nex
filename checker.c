c83faadb71b43299bed3db42ae8b7b2fe484b1b4
Ramesh-nex <rameshsaravanan16091996@gmail.com>
2024-07-22 14:37:27 +0530

    German support

--- checker.c
+++ checker.c
@@ -3,25 +3,48 @@
 
 int language = 0;
 
-void PrintMessage(const char* msg) {
-    if (language == 0) {
-        printf("EN: %s\n", msg);
-    } 
-}
-int OutOfRange(float val, float min, float max ,const char* msg)
+// Function to print messages based on language selection
+void PrintMessage(const char* msg_en, const char* msg_de)
 {
-  if (val < min || val > max) 
-  {
-    PrintMessage(msg);
-    return 0;
-  }
+    switch (language) 
+    {
+        case 0:
+            printf("EN: %s\n", msg_en);
+            break;
+        case 1:
+            printf("DE: %s\n", msg_de);
+            break;
+        default:
+            printf("Language not supported\n");
+            break;
+    }
+}
+
+// Function to check if a value is out of specified range
+int OutOfRange(float val, float min, float max, const char* msg_en, const char* msg_de) 
+{
+    if (val < min || val > max)
+    {
+        PrintMessage(msg_en, msg_de);
+        return 0;
+    }
     return 1;
 }
+
 int batteryIsOk(float temperature, float soc, float chargeRate)
 {
-   return OutOfRange(temperature,0,45,"Temperature out of range!")
-      && OutOfRange(soc,20,80,"SOC out of range!")
-      && OutOfRange(chargeRate,0,0.8,"Charge Rate out of range!");
+  // Messages in English and German
+    const char* msg_temp_en = "Temperature out of range!";
+    const char* msg_soc_en = "SOC out of range!";
+    const char* msg_charge_en = "Charge Rate out of range!";
+    
+    const char* msg_temp_de = "Temperatur außerhalb des zulässigen Bereichs!";
+    const char* msg_soc_de = "SOC außerhalb des zulässigen Bereichs!";
+    const char* msg_charge_de = "Ladestrom außerhalb des zulässigen Bereichs!";
+    
+    return OutOfRange(temperature, 0, 45, msg_temp_en, msg_temp_de)
+        && OutOfRange(soc, 20, 80, msg_soc_en, msg_soc_de)
+        && OutOfRange(chargeRate, 0, 0.8, msg_charge_en, msg_charge_de);
 }
 
 int main() {
