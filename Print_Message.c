e0a87dada0278db44df4dcd0abc71af4766632af
Ramesh-nex <rameshsaravanan16091996@gmail.com>
2024-07-22 15:03:24 +0530

    Update and rename Generate to Print_Message.c

---
+++ Print_Message.c
@@ -0,0 +1,16 @@
+void PrintMessage(const char* msg_en, const char* msg_de)
+{
+    switch (language) 
+    {
+        case 0:
+            printf("EN: %s\n", msg_en);
+            break;
+        case 1:
+            printf("DE: %s\n", msg_de);
+            break;
+        default:
+            printf("Language not supported\n");
+            break;
+    }
+}
+
